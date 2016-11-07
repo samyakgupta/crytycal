76f2e6301460a5613ce5a1e8e00ef5d4239f3d8f
Raamish Malik <raamish.malik@gmail.com>
2016-11-08 02:39:39 +0530

    updated again

--- checker.py
+++ checker.py
@@ -52,18 +52,21 @@ def match(output):
         return 404
 
 file = argv[1]
-filename, file_extension = os.path.splittext(file);
-lang = file_extension
+filename, file_extension = os.path.splitext(file);
+garbage,lang = file_extension.split(".")
+# print file
+# print filename
+# print lang
 
 testin = argv[2]
 testout = argv[3]
 timeout = '1' # secs
 
 if lang == 'c':
-	print(codes[compile(file,'c')])
-	print (codes[run(filename,testin,timeout,lang)])
-	print (match(testout))
+    print (codes[compile(file,'c')])
+    print (codes[run('add',testin,timeout,lang)])
+    print (match(testout))
 elif lang == 'java':
-	print(codes[compile(file,'java')])
-	print (codes[run(filename,testin,timeout,lang)])
-	print (match(testout))
+    print (codes[compile(file,'java')])
+    print (codes[run(file,testin,timeout,lang)])
+    print (match(testout))
